e631b43785ba67aa40d6dee7eb9efefe0c178369
LightZhang <978854603@qq.com>
2019-03-29 09:59:26 +0800

    修改 部分

--- src/App.vue
+++ src/App.vue
@@ -2,87 +2,89 @@
   <div id="app">
     <div class="head">
       <div class="head-top">
-        <Row>
-          <Col span="8" style="text-align: left;">
-            <img class="login-bg" src="./assets/img/logo-min.png">
-          </Col>
-          <Col span="10">
-            <h1>鑫淳云平台管理会计理念ERP系统</h1>
-          </Col>
-          <Col offset="4" span="2">
+        <ul>
+          <!-- <li> <img class="login-bg" src="./assets/img/logo-min.png"></li>
+
+          <li> <img class="title-bg" src="./assets/img/title.jpg"> </li>
+
+          <li class="basic-li">
             <Tag color="warning" class="basic-information">基础资料</Tag>
-          </Col>
-        </Row>
+          </li> -->
+        </ul>
       </div>
 
       <div class="head-bottom">
         <ul class="top-menu">
           <li v-for="menu in topMenus" :key="menu.key">
             <Card>
-              <h3>{{menu.name}}</h3>
+              <Icon :type="menu.Icon"></Icon>
+              <label>{{menu.name}}</label>
             </Card>
           </li>
         </ul>
         <div class="right-select">
-          <div style="width:300px;display: inline-block;">
-            <Form :label-width="80">
-              <FormItem label="帐套选择：">
-                <Select v-model="company">
-                  <Option value="上海鑫淳财务管理股份有限公司">上海鑫淳财务管理股份有限公司</Option>
-                  <Option value="上海鑫淳财务管理股份有限公司">上海鑫淳财务管理股份有限公司</Option>
-                  <Option value="上海鑫淳财务管理股份有限公司">上海鑫淳财务管理股份有限公司</Option>
-                </Select>
-              </FormItem>
-            </Form>
+          <div style="width:400px;display: inline-block;">
+
+            <Row>
+              <Col span="8">
+              <label class="lable-text">套账选择：</label>
+              </Col>
+              <Col span="16">
+              <Select v-model="company">
+                <Option value="上海鑫淳财务管理股份有限公司">上海鑫淳财务管理股份有限公司</Option>
+                <Option value="上海鑫淳财务管理股份有限公司">上海鑫淳财务管理股份有限公司</Option>
+                <Option value="上海鑫淳财务管理股份有限公司">上海鑫淳财务管理股份有限公司</Option>
+              </Select>
+              </Col>
+            </Row>
           </div>
         </div>
       </div>
     </div>
     <div class="main">
       <div class="left-menu">
-        <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
-          <Submenu name="1">
-            <template slot="title">
-              <Icon type="ios-navigate"></Icon>Item 1
-            </template>
-            <MenuItem name="1-1">Option 1</MenuItem>
-            <MenuItem name="1-2">Option 2</MenuItem>
-            <MenuItem name="1-3">Option 3</MenuItem>
-          </Submenu>
-          <Submenu name="2">
-            <template slot="title">
-              <Icon type="ios-keypad"></Icon>Item 2
-            </template>
-            <MenuItem name="2-1">Option 1</MenuItem>
-            <MenuItem name="2-2">Option 2</MenuItem>
-          </Submenu>
-          <Submenu name="3">
-            <template slot="title">
-              <Icon type="ios-analytics"></Icon>Item 3
-            </template>
-            <MenuItem name="3-1">Option 1</MenuItem>
-            <MenuItem name="3-2">Option 2</MenuItem>
-          </Submenu>
+        <Menu theme="light" width="auto" :open-names="['财务管理']">
+          <div class="menu-li" v-for="menu in leftMenus " :key="menu.ID">
+            <Submenu :name="menu.Name" v-if="menu.Children.length>0">
+              <template slot="title">
+                <Icon :type="menu.Icon"></Icon>
+                <label>{{menu.Name}}</label>
+              </template>
+              <MenuItem class="son-menu-li" :name="sonMenu.Name" v-for="sonMenu in menu.Children" :key="sonMenu.ID">
+              {{sonMenu.Name}}
+              </MenuItem>
+            </Submenu>
+            <MenuItem v-else :name="menu.Name">
+            <Icon :type="menu.Icon"></Icon>
+            <label>{{menu.Name}}</label>
+            </MenuItem>
+          </div>
+
         </Menu>
       </div>
       <div class="content-box"></div>
     </div>
-    <div class="bottom"></div>
+    <div class="bottom">
+
+    </div>
   </div>
 </template>
 
 <script>
 import topmenus from "../public/static/topmenu.json";
+import leftmenus from "../public/static/leftmenu.json";
 export default {
   name: "app",
   data() {
     return {
       topMenus: [],
+      leftMenus: [],
       company: ""
     };
   },
-  created: function() {
+  created: function () {
     this.topMenus = topmenus;
+    this.leftMenus = leftmenus;
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import iView from 'iview';
+import './assets/css/base.css';
 import 'iview/dist/styles/iview.css';
 import './assets/css/app.less';
 
